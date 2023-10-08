2efebd04d88cc555823b2c9b599d7d4e600decff
akorovai <aktey.main@gmail.com>
2023-10-08 15:56:43 +0200

    feat: create Student entity and StudentRepository

---
+++ src/main/java/entity/Student.java
@@ -0,0 +1,25 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+}
